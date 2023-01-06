5aabecca79bb7aad8c89a3095a4a152f113ff5d7
DropBox3 <Casper.h.blichfeldt@live.dk>
2023-01-06 15:46:54 +0100

    Fra GUItest til Matador

--- src/main/java/org/example/MonopolyGUI.java
+++ src/main/java/org/example/MonopolyGUI.java
@@ -3,18 +3,16 @@ import gui_fields.GUI_Field;
 import gui_fields.GUI_Player;
 import gui_main.GUI;
 
-import java.io.*;
-
 public class MonopolyGUI {
-
-    public static GUI gui = new GUI();
+    Terning terning1 = new Terning();
+    Terning terning2 = new Terning();
+    private GUI gui = new GUI();
     static GUI_Player[] player = new GUI_Player[4];
     GUI_Field start = gui.getFields()[0];
     public GUI GUIstartup(){
         return gui;
     }
     public void GUIPlayerstart(){
-        bræt.Board();
         String playeramountstring = gui.getUserSelection("How many players?", "2", "3", "4");
         int playeramount = Integer.parseInt(playeramountstring);
         for (int i = 1; i < playeramount + 1; i++){
@@ -23,10 +21,9 @@ public class MonopolyGUI {
             gui.addPlayer(player[i - 1]);
             player[i - 1].getCar().setPosition(start);
         }
-
     }
 
-    public void Updatebalance(int leje, int id){ // Updatebalance, fungerer på den måde at man sætter to parameter den skal bruge. en leje og id på personen, som er player: 0, 1, 2 ,
+    public void Updatebalance(int leje, int id){ // Updatebalance, fungerer på den måde at man sætter to parameter den skal bruge. En leje og id på personen, som er player: 0, 1, 2 ,
                                                             // Koden er derfor lavet på den måde, at man kalder på playerens id, og derfra sætter playerens balance, udfra hvad playerens
         player[id].setBalance(player[id].getBalance()+leje); // balance er i forvejen, og ligger lejen til eller trækker fra, alt afhængig af om det leje man skal betale eller få
     }
@@ -35,6 +32,20 @@ public class MonopolyGUI {
 
     }
 
+    public void showMessage(String msg){
+        gui.showMessage(msg);
+
+    }
+
+    public void roll(){
+        terning1.roll();
+        terning2.roll();
+    }
+
+    public void getFace(){
+        gui.setDice(terning1.getFaceValue(), terning2.getFaceValue());
+
+    }
 
 
 
