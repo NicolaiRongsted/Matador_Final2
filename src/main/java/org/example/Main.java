929aad23ffbf01ffdf7829d2fd4e79dd33e8bbef
MarcusPrehnChang <s224294@student.dtu.dk>
2023-01-06 10:13:59 +0100

    GameController start og Gui tests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,4 @@
 package org.example;
-import java.util.Scanner;
-
-import gui_codebehind.GUI_BoardController;
-import gui_fields.GUI_Board;
-import gui_fields.GUI_Field;
-import gui_fields.GUI_Player;
-import gui_main.GUI;
-import gui_codebehind.GUI_FieldFactory;
-import gui_codebehind.GUI_Center;
-
 
 public class Main {
     public static void main(String[] args) {
