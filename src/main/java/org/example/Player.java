7480cfc47f521a391e1a90deadf1518b7bf23e81
MarcusPrehnChang <s224294@student.dtu.dk>
2023-01-08 06:46:35 +0100

    GUItest til Master

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -2,12 +2,16 @@ package org.example;
 
 public class Player {
     private int position;
-    public Player(int position){
+    private String name;
+    public Player(int position, String name){
         this.position = position;
+        this.name = name;
     }
     public void setPosition(int terningkast){
         position =+ terningkast;
     }
     public int getPosition(){return position;}
-
+    public String getName(){
+        return name;
+    }
 }
