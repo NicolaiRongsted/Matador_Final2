3321901b19c491c65c45646a1f4f5e6fd14d512f
NicolaiRongsted <Nicolai.Rongsted@gmail.com>
2023-01-08 16:54:58 +0100

    Chancekort (mergekonflikt fix)

--- src/main/java/org/example/Gamestates.java
+++ src/main/java/org/example/Gamestates.java
@@ -1,7 +1,12 @@
 package org.example;
 
-public class Gamestates extends Gamestate{
-    public Gamestates(String name){
-        super(name);
+public class Gamestates {
+    public Player[] players;
+    public boolean Forceroll = false;
+    public int[] positions;
+
+    public Gamestates(boolean Forceroll, int[] positions, int[] balance){
+        this.Forceroll = Forceroll;
+        positions = new int[4];
     }
 }
