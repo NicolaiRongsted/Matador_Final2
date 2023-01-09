7b79cb6374eac3072327e63ded8acc1c6d3287d7
NicolaiRongsted <Nicolai.Rongsted@gmail.com>
2023-01-09 12:08:50 +0100

    Chancekort (Final)

--- src/main/java/org/example/ChanceKort.java
+++ src/main/java/org/example/ChanceKort.java
@@ -5,10 +5,15 @@ public class ChanceKort extends Kort {
     int cardNumber;
     int cardValue;
     int cardPosition;
-    public ChanceKort(String description, KortType chanceKort, int cardNumber, int cardValue, int cardPosition) { //her er vores chancekort som public med int's til vlaue og number
+    int caseNumber;
+
+    public ChanceKort(String description, KortType chanceKort, int cardNumber, int cardValue, int cardPosition, int caseNumber) { //her er vores chancekort som public med int's til vlaue og number
         super(description, chanceKort);
         this.cardNumber = cardNumber;
         this.cardValue = cardValue;
         this.cardPosition = cardPosition;
+        this.caseNumber = caseNumber;
+
+
     }
 }
