c9f1c2a758a7c3bd0e4900b830be8f04ef2196f9
MarcusPrehnChang <s224294@student.dtu.dk>
2023-01-10 19:08:44 +0100

    Gamestate case til og med 7 burde virke

--- src/main/java/org/example/Gamestate.java
+++ src/main/java/org/example/Gamestate.java
@@ -2,11 +2,23 @@ package org.example;
 
 public class Gamestate extends DefaultGamestate {
     int[] forceRoll;
-    public Gamestate(int caseNumber, int[] forceRolls){
+    int positions;
+    private boolean forceroll;
+    private boolean forceposition;
+    private boolean forcedouble;
+
+    public Gamestate(int caseNumber, boolean forceroll, boolean forcedouble, int[] forceRolls, boolean forceposition, int positions){
         super(caseNumber);
         this.forceRoll = forceRolls;
+        this.positions = positions;
+        this.forceroll = forceroll;
+        this.forceposition = forceposition;
+        this.forcedouble = forcedouble;
     }
     public int[] getForceRoll(){return forceRoll;}
+    public int getPosition(){
+        return positions;
+    }
 
 
 }
