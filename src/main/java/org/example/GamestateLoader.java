a2e9ffa114cfdf7f696dfd055b4269d2b61bb2d0
MarcusPrehnChang <s224294@student.dtu.dk>
2023-01-10 19:08:26 +0100

    Gamestate case til og med 7 burde virke

--- src/main/java/org/example/GamestateLoader.java
+++ src/main/java/org/example/GamestateLoader.java
@@ -2,12 +2,12 @@ package org.example;
 
 public class GamestateLoader {
     private static Gamestate[] gamestates = {
-            new Gamestate(3, new int[]{3, 3}), //
-            //new Gamestate(4), //
-            //new Gamestate(5), //
-            //new Gamestate(6), //
-            //new Gamestate(7), //
-            //new Gamestate(8), //
+            new Gamestate(3,true, false, new int[]{3, 3}, false,0), //
+            new Gamestate(4, true, false, new int[]{10}, true, 20), //
+            new Gamestate(5, true, false, new int[]{7,7}, false, 0), //
+            new Gamestate(6, true, false, new int[]{11}, true, 32), //
+            new Gamestate(7, true,false, new int[]{3}, true, 39), //
+            new Gamestate(8, false, true, new int[]{0}, false, 0), //
             //new Gamestate(9), //
             //new Gamestate(10), //
 
