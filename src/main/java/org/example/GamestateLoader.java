708c3473be259d02455da013d240d4dbc8ad781c
MarcusPrehnChang <s224294@student.dtu.dk>
2023-01-11 10:36:19 +0100

    Gamestate case til og med 9 vioker

--- src/main/java/org/example/GamestateLoader.java
+++ src/main/java/org/example/GamestateLoader.java
@@ -8,7 +8,7 @@ public class GamestateLoader {
             new Gamestate(6, true, false, new int[]{11}, true, 32, false), //
             new Gamestate(7, true,false, new int[]{4,4}, true, 0, false), //
             new Gamestate(8, false, true, new int[]{10}, false, 0, false ), //Slaa 2 ens
-            new Gamestate(9, false, true, new int[]{10}, true, 20, true) //2 ens uid af faengsel
+            new Gamestate(9, false, true, new int[]{10}, true, 10, true) //2 ens uid af faengsel
             //new Gamestate(10), //
 
     };
