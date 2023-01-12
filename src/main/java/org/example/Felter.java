253cf10103bf3a126d02baf69ced3974e9cc8f5a
MarcusPrehnChang <s224294@student.dtu.dk>
2023-01-12 11:30:12 +0100

    Huse start, mangler implementation

--- src/main/java/org/example/Felter.java
+++ src/main/java/org/example/Felter.java
@@ -3,26 +3,28 @@ import gui_main.GUI;
 import static org.example.MonopolyGUI.gui;
 
 public class Felter {
-    private int leje;
-    private int leje1;
-    private int leje2;
-    private int leje3;
-    private int leje4;
+    private int[] leje;
     private int housecost;
     private int position;
     private int owner = 5;
+    private int rentamounts = 5;
+    private int houses = 0;
     public Felter(int housecost, int leje, int leje1, int leje2, int leje3, int leje4, int position){
-        this.leje = leje;
-        this.leje1 = leje1;
-        this.leje2 = leje2;
-        this.leje3 = leje3;
-        this.leje4 = leje4;
+        this.leje = new int[]{leje, leje1, leje2, leje3, leje4};
         this.housecost = housecost;
         this.position = position;
     }
 
     public int getOwner(){return owner;}
     public void setOwner(int owner){this.owner = owner;}
+    public int getRent(){
+        return leje[houses];
+    }
 
-
+    public void setHouses(int amount){
+        houses = amount;
+    }
+    public int getHouses(){
+        return houses;
+    }
 }
