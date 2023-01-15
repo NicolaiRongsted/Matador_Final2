d18d8fb6da84ce50d80dd048d222f615ee2903e3
MarcusPrehnChang <s224294@student.dtu.dk>
2023-01-15 19:06:42 +0100

    fikset nogen smaa ting

--- src/main/java/org/example/GameController.java
+++ src/main/java/org/example/GameController.java
@@ -285,5 +285,6 @@ public class GameController {
         ownable.setOwnerName(game.getName(player));
         ownable.setBorder(game.player[player].getPrimaryColor());
         players[player].setOwned(position);
+        bræt.felter[position].setOwner(player);
     }
 }
--- src/main/java/org/example/MonopolyGUI.java
+++ src/main/java/org/example/MonopolyGUI.java
@@ -1,10 +1,7 @@
 package org.example;
-
 import gui_fields.GUI_Field;
-import gui_fields.GUI_Ownable;
 import gui_fields.GUI_Player;
 import gui_main.GUI;
-
 
 import java.awt.*;
 import java.io.*;
@@ -12,8 +9,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MonopolyGUI {
-
-    GUI_Ownable Sut;
     private GamestateLoader gamestateLoader;
     public Gamestate gamestates[] = gamestateLoader.getGamestates();
     public static GUI gui = new GUI();
@@ -21,25 +16,27 @@ public class MonopolyGUI {
     static GUI_Player[] player;
     GUI_Field start = gui.getFields()[0];
     public static Player[] players;
-
-    public GUI GUIstartup() {
+    public GUI GUIstartup(){
         return gui;
     }
-
     public static int playeramount;
-
-    public void GUIPlayerstart(boolean testscenario, int startposition) {
+    public int aiamount;
+    public void GUIPlayerstart(boolean testscenario, int startposition){
         bræt.Board();
         start = gui.getFields()[startposition];
-        if (!testscenario) {
-            String playeramountstring = gui.getUserSelection("How many players?", "2", "3", "4");
+        if(!testscenario){
+            String playeramountstring = gui.getUserSelection("How many players?", "1", "2", "3", "4");
             playeramount = Integer.parseInt(playeramountstring);
-        } else {
+        }else{
             playeramount = 2;
+        }
+        boolean ai = Yes_or_no("Du har valgt " + playeramount + "Antal spillere, vil du gerne fylde resten af pladserne med AI?");
+        if(ai){
+            playeramount = 4;
         }
         player = new GUI_Player[playeramount];
         players = new Player[playeramount];
-        for (int i = 0; i < playeramount; i++) {
+        for (int i = 0; i < playeramount; i++){
             String playername = gui.getUserString("Whats the name of player " + (i + 1) + "?");
             String chosenColor = gui.getUserSelection("Hvilken farve vil du gerne have?", colors.toArray(new String[colors.size()]));
             colors.remove(chosenColor);
@@ -52,69 +49,65 @@ public class MonopolyGUI {
             }else {player[i].getCar().setPrimaryColor(Color.black);}
             player[i].getCar().setPosition(start);
         }
+
     }
 
-    public void Updatebalance(int leje, int id) { // Updatebalance, fungerer på den måde at man sætter to parameter den skal bruge. En leje og id på personen, som er player: 0, 1, 2 ,
-        // Koden er derfor lavet på den måde, at man kalder på playerens id, og derfra sætter playerens balance, udfra hvad playerens
-        player[id].setBalance(player[id].getBalance() + leje); // balance er i forvejen, og ligger lejen til eller trækker fra, alt afhængig af om det leje man skal betale eller få
-        if (player[id].getBalance() < 0) {
+    public void Updatebalance(int leje, int id){ // Updatebalance, fungerer på den måde at man sætter to parameter den skal bruge. En leje og id på personen, som er player: 0, 1, 2 ,
+                                                            // Koden er derfor lavet på den måde, at man kalder på playerens id, og derfra sætter playerens balance, udfra hvad playerens
+        player[id].setBalance(player[id].getBalance()+leje); // balance er i forvejen, og ligger lejen til eller trækker fra, alt afhængig af om det leje man skal betale eller få
+        if(player[id].getBalance() < 0){
             gui.showMessage("Spilleren " + player[id].getName() + " Er desværre gået bankeråt og er derfor ude af spillet!");
             players[id].setActive();
-            clearOwner();
             int[] array = players[id].getOwned();
-            for (int i = 0; i < array.length; i++) {
+            for (int i = 0; i < array.length; i++){
             }
             playeramount = playeramount - 1;
         }
     }
 
-    public void Updateposition(int ID, int terningkast) { //Opdater positionen på gui
+    public void Updateposition(int ID, int terningkast){ //Opdater positionen på gui
         players[ID].setPosition(players[ID].getPosition() + terningkast);
-        if (players[ID].getPosition() >= 40) {
+        if (players[ID].getPosition() >= 40){
             players[ID].setPosition(players[ID].getPosition() % 40);
             Updatebalance(4000, ID);
         }
         GUI_Field field = gui.getFields()[players[ID].getPosition()];
         player[ID].getCar().setPosition(field);
     }
-
-    public void Setposition(int ID, int position) {
+    public void Setposition(int ID, int position){
         players[ID].forcePosition(position);
         GUI_Field field = gui.getFields()[position];
         player[ID].getCar().setPosition(field);
     }
 
 
-    public void showMessage(String msg) { //GUI er lavet statisk og kan tilgås fra andre klasser. og der er derfor ikke behov for denne.
+    public void showMessage(String msg){ //GUI er lavet statisk og kan tilgås fra andre klasser. og der er derfor ikke behov for denne.
         gui.showMessage(msg);
     }
 
-    public void showDice(int dice1, int dice2) { //skal hedde noget andet end getFace da det ikke er det den gør.
+    public void showDice(int dice1, int dice2){ //skal hedde noget andet end getFace da det ikke er det den gør.
         gui.setDice(dice1, dice2);
     }
-
-    public String getName(int id) {
+    public String getName(int id){
         return players[id].getName();
     }
 
 
-    public boolean Yes_or_no(String message) {
+    public boolean Yes_or_no(String message){
         String yes = "Yes";
         String no = "No";
         String input = gui.getUserSelection(message, yes, no);
         return input.equals(yes);
     }
 
-    public void displayChanceCard(String chancekort) {
+    public void displayChanceCard(String chancekort){
         gui.displayChanceCard(chancekort);
 
     }
 
-    public boolean checkActivePlayer(int id) {
+    public boolean checkActivePlayer(int id){
         return players[id].getActive();
     }
+    public void clearOwner(){}
 
-    public void clearOwner() {
-        Sut.setOwnerName(null);
-    }
 }
