b57368e57dfed46be43230aff906abd735d82421
DropBox3 <Casper.h.blichfeldt@live.dk>
2023-01-17 16:32:52 +0100

    I gang med at man kan sælge grunde når man løber tør for penge

--- src/main/java/org/example/MonopolyGUI.java
+++ src/main/java/org/example/MonopolyGUI.java
@@ -76,18 +76,15 @@ public class MonopolyGUI {
                                                             // Koden er derfor lavet på den måde, at man kalder på playerens id, og derfra sætter playerens balance, udfra hvad playerens
         player[id].setBalance(player[id].getBalance()+leje); // balance er i forvejen, og ligger lejen til eller trækker fra, alt afhængig af om det leje man skal betale eller få
         if(player[id].getBalance() < 0){
-            boolean sell = Yes_or_no("Du har ikke flere penge vil du sælge nogle af dine grunde");
-            if(sell){
-                gui.getUserSelection("Hvilken grund vil du sælge?", "");
+                int[] array1 = players[id].getOwned();
+                gui.getUserSelection("Hvilken grund vil du sælge?", array1.toString());
                 //sellField();
-            }
-            if(!sell) {
                 gui.showMessage("Spilleren " + player[id].getName() + " Er desværre gået bankeråt og er derfor ude af spillet!");
                 int[] array = players[id].getOwned();
                 for (int i = 0; i < array.length; i++) {
                     clearOwner(array[i]);
                 }
-            }
+
             players[id].setActive();
             playeramount = playeramount - 1;
         }
@@ -145,11 +142,10 @@ public class MonopolyGUI {
         System.out.println("Spiller ejer ikke noget");
     }
 
-    public void sellField (int position){
+    public void sellField (int position, int id){
         if (position !=0){
-            GUI_Ownable Sut = (GUI_Ownable) gui.getFields()[position];
-            Sut.setOwnerName(null);
-            bræt.felter[position].setOwner(5);
+            player[id].setBalance(player[id].getBalance()+bræt.felter[position].getPrice());
+            clearOwner(position);
         }
     }
 }
